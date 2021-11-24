bf6006fb2f58fa84dbceca9c632a7d1cf1795b5e
bergi <s.berke@netzkolchose.de>
2021-11-25 00:27:07 +0100

    fix geo_point transfer

--- web/park_api/tests/test_data.py
+++ web/park_api/tests/test_data.py
@@ -1,3 +1,5 @@
+from django.contrib.gis.geos import Point
+
 from .base import *
 
 
@@ -7,4 +9,9 @@ class TestData(TestBase):
         snapshot = self.load_data("snapshot01.json")
 
         data_models = store_snapshot(snapshot)
-        print(data_models)
+
+        self.assertEqual(207, data_models[0].capacity)
+        self.assertEqual(197, data_models[0].num_free)
+        self.assertEqual(197 / 207 * 100., data_models[0].percent_free)
+
+        self.assertEqual((7.341648, 51.652461), data_models[0].lot.geo_point.tuple)
--- web/park_data/models/_store.py
+++ web/park_data/models/_store.py
@@ -1,6 +1,7 @@
 from typing import List
 
 from django.db import transaction
+from django.contrib.gis.geos import Point
 
 from .parking_pool import ParkingPool
 from .parking_lot import ParkingLot
@@ -33,6 +34,8 @@ def store_snapshot(snapshot: dict) -> List[ParkingData]:
             kwargs["lot_id"] = kwargs.pop("id")
             kwargs["pool"] = pool_model
             kwargs["max_capacity"] = lot["capacity"]
+            if not (lot["latitude"] is None or lot["longitude"] is None):
+                kwargs["geo_point"] = Point(lot["longitude"], lot["latitude"])
             lot_model = ParkingLot.objects.create(**kwargs)
 
         kwargs = {key: value for key, value in lot.items() if hasattr(ParkingData, key)}
