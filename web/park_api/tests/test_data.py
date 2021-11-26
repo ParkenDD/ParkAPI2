d1429405638309acf2aae61465f525951657c4a4
bergi <s.berke@netzkolchose.de>
2021-11-26 11:22:45 +0100

    add a test of data which made problems in live mode but which of course does not reveal the problem in the test

--- web/park_api/tests/test_data.py
+++ web/park_api/tests/test_data.py
@@ -15,3 +15,11 @@ class TestData(TestBase):
         self.assertAlmostEqual(197 / 207 * 100., data_models[0].percent_free, places=2)
 
         self.assertEqual((7.341648, 51.652461), data_models[0].lot.geo_point.tuple)
+
+        snapshot = self.load_data("snapshot02.json")
+        data_models = store_snapshot(snapshot)
+
+        self.assertEqual(70, data_models[0].capacity)
+        self.assertEqual("nodata", data_models[0].status)
+        self.assertEqual(None, data_models[0].num_free)
+        self.assertEqual(None, data_models[0].percent_free)
