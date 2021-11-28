e14dcd1a9bf2a887c5cc5f0cd07ac1cc39c6ab80
bergi <s.berke@netzkolchose.de>
2021-11-28 17:57:29 +0100

    add Braunschweig scraper

--- web/scrapers/builtin/util/soup.py
+++ web/scrapers/builtin/util/soup.py
@@ -2,11 +2,21 @@ import bs4
 
 
 def get_soup_text(tag: bs4.Tag) -> str:
+    """
+    Get the text from a soup element but keep the <br> new-lines.
+    All lines are .strip()ed and empty lines are dropped.
+
+    This actually changes the soup tree!
+
+    :param tag: a soup element
+    :return: str
+    """
     DELIMITER = "#!-%br-DeLiMiTtEr"
     for br in tag.find_all("br"):
         br.replaceWith(DELIMITER)
 
-    return tag.text.replace(DELIMITER, "\n").strip()
+    text = tag.text.replace(DELIMITER, "\n").strip()
+    return "\n".join(filter(bool, (line.strip() for line in text.splitlines())))
 
 
 
